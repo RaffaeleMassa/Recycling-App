1b5b85f5335e3b495534d1d4fd465c6d2312f541
RaffaeleMassa <76770497+RaffaeleMassa@users.noreply.github.com>
2021-05-06 12:47:49 +0200

    Create show.blade.php

---
+++ My project/Recycle/resources/views/weeks/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+  
+@section('mainContent')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Day</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('weeks.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Day:</strong>
+                {{ $week->Day }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Hour:</strong>
+                {{ $week->Hour }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Recycle:</strong>
+                {{ $week->Recycle }}
+            </div>
+        </div>
+    </div>
+@endsection
