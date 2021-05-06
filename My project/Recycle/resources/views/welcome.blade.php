b651b7c3c3d2827bf3f2cf6bd7b3c396e3b62b0d
RaffaeleMassa <76770497+RaffaeleMassa@users.noreply.github.com>
2021-05-06 12:47:52 +0200

    Create welcome.blade.php

---
+++ My project/Recycle/resources/views/welcome.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+                
+            }
+
+            .title {
+                font-size: 84px;
+                text-shadow: 2px 2px;
+            }
+
+            .links > a {
+                display: inline-block;
+                border-radius: 4px;
+                background-color: #f4511e;
+                border: none;
+                color: #FFFFFF;
+                text-align: center;
+                font-size: 40px;
+                padding: 20px;
+                width: 200px;
+                height: 50px;
+                margin: 25px;
+                text-decoration: none;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+     
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Recycle
+                </div>
+
+                <div class="links">
+                    <a class="button" href="{{ route('weeks.create') }}">Create</a> 
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
